3f119bf00e9398d7c8e5f63d65ca13703a56ad0a
JMoore31 <154471085+JMoore31@users.noreply.github.com>
2024-02-26 21:18:20 +0000

    Made betterer

--- src/yaml.rs
+++ src/yaml.rs
@@ -1,13 +1,14 @@
+
 use serde::{Deserialize, Serialize};
 use serde_yaml::{self};
-use std::io::Write;
+use std::io::{BufRead, BufReader,Write};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Releases {
     pub releases: Vec<Release>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Default)]
 pub struct Release {
     pub name: String,
     pub chart: String,
@@ -60,4 +61,25 @@ pub fn yaml_to_file(
     let _ = &file.write_all(separator);
     serde_yaml::to_writer(&file, &release_list).unwrap();
     Ok(())
+}
+
+
+pub fn file_to_releases()-> Releases{
+    let file = std::fs::OpenOptions::new().read(true).open("input.txt").unwrap();
+    let reader = BufReader::new(&file);
+    let mut releases = Releases::new();
+    let mut release = Release::default();
+    for line in reader.lines(){
+        let split = line.unwrap();
+        let split = split.split_whitespace();
+        let pair = split.collect::<Vec<&str>>();
+        match pair[0] {
+            "name" => release.name = (pair[1]).to_string(),
+            "chart" => release.chart = (pair[1]).to_string(),
+            "namespace" => release.namespace = (pair[1]).to_string(),
+            _ => println!("Unidentified key in pair"),
+        }
+    }
+    releases.add_release(release);
+    releases
 }
