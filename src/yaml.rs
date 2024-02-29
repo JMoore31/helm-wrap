e84345069610eada91525f4be3babb1be95d992a
JMoore31 <julian.admoore@gmail.com>
2024-02-29 21:36:07 +0000

    added values option

--- src/yaml.rs
+++ src/yaml.rs
@@ -1,7 +1,6 @@
-
 use serde::{Deserialize, Serialize};
 use serde_yaml::{self};
-use std::io::{BufRead, BufReader,Write};
+use std::io::{BufRead, BufReader, Write};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Releases {
@@ -14,6 +13,7 @@ pub struct Release {
     pub chart: String,
     pub namespace: String,
     pub needs: Vec<String>,
+    pub values: Vec<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -67,14 +67,16 @@ pub fn yaml_to_file(helmfile: Helmfile) -> Result<(), serde_yaml::Error> {
     Ok(())
 }
 
-
-pub fn file_to_releases()-> Helmfile{
-    let file = std::fs::OpenOptions::new().read(true).open("input.txt").unwrap();
+pub fn file_to_releases() -> Helmfile {
+    let file = std::fs::OpenOptions::new()
+        .read(true)
+        .open("input.txt")
+        .unwrap();
     let reader = BufReader::new(&file);
     let mut releases = Releases::new();
     let mut release = Release::default();
     let mut repositories = Repositories::new();
-    for line in reader.lines(){
+    for line in reader.lines() {
         let split = line.unwrap();
         let split = split.split_whitespace();
         let pair = split.collect::<Vec<&str>>();
@@ -83,23 +85,28 @@ pub fn file_to_releases()-> Helmfile{
             "chart" => release.chart = (pair[1]).to_string(),
             "namespace" => release.namespace = (pair[1]).to_string(),
             "needs" => {
-                for n in 1..pair.len(){
+                for n in 1..pair.len() {
                     release.needs.push(pair[n].to_string());
                 }
             }
-            "repository" => repositories.add_repository(Repository{
+            "values" => {
+                for n in 1..pair.len() {
+                    release.values.push(pair[n].to_string());
+                }
+            }
+            "repository" => repositories.add_repository(Repository {
                 name: pair[1].to_string(),
                 url: pair[2].to_string(),
             }),
             "---" => {
                 releases.add_release(release);
-                release = Release::default(); 
+                release = Release::default();
             }
             _ => println!("Unidentified key in pair"),
         }
     }
-    
-    println!("{:?}",releases);
+
+    println!("{:?}", releases);
     let helmfile = Helmfile {
         releases: releases,
         repositories: repositories,
